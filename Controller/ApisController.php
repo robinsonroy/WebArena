837a78fb3762ec244ef4996674ff84461867b887
Greg <gportier@ece.com>
2014-11-17 17:52:20 +0100

    ApisController

--- Controller/ApisController.php
+++ Controller/ApisController.php
@@ -9,6 +9,7 @@ App::uses('AppController', 'Controller');
 
 class ApisController extends AppController
 {
+    public $uses = array('Player', 'Fighter', 'Event');
 
     public function index()
     {
@@ -22,25 +23,26 @@ class ApisController extends AppController
 
         $this->layout='ajax';
 
-        $this->set('datas', $this->Fighter->find('all'));
+        $this->set('datas', $this->Fighter->findById($id));
 
 
 
 
     }
-    public function fighterdomove()
+    public function fighterdomove($id)
     {
+
         $this->layout='ajax';
-        $this->set('datas',$this->Fighter->find('all'));
+        $this->set('datas',$this->Fighter->findById($id));
 
 
     }
 
-    public function fighterdoattack()
+    public function fighterdoattack($id)
     {
 
         $this->layout='ajax';
-        $this->set('datas',$this->Fighter->find('all'));
+        $this->set('datas',$this->Fighter->findById($id));
 
     }
 }
