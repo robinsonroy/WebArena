5cea4843628c1a8a3cf6a5bfa2736cd3f0bb1473
Greg <gportier@ece.com>
2014-11-17 14:55:15 +0100

    MAJ 2.0

--- Controller/ArenaController.php
+++ Controller/ArenaController.php
@@ -5,7 +5,7 @@
  * Date: 07/11/14
  * Time: 17:47
  */
-    App::uses('AppController', 'Controller');
+App::uses('AppController', 'Controller');
 
 /**
  * Main controller of our small application
@@ -31,184 +31,101 @@ class ArenaController extends AppController
 
     public function character()
     {
-        $this->set('raw',$this->Fighter->findById(1));
+        $this->set('raw', $this->Fighter->findById(1));
     }
 
     public function diary()
     {
-        $this->set('raw',$this->Event->find());
+        $this->set('raw', $this->Event->find());
     }
 
     public function login()
     {
 
-    $this->Player->loginplayer($this->request->data['login']['Login'],$this->request['login']['password']);
+        $this->Player->loginplayer($this->request->data['login']['Login'], $this->request['login']['password']);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     public function sight()
     {
-<<<<<<< HEAD
 
-
-        if ($this->request->is('post'))
-        {            pr($this->request->data);        }
-
-
-         $this->set('raw',$this->Fighter->find('all'));
-         $this->Fighter->doMove(1, $this->request->data['Fightermove']['direction']);
-
-         $this->Fighter->doAttack($this->request->data['Fighterattack']['id'],$this->request->data['Fighterattack']['id2'],$this->request->data['Fighterattack']['direction']);
-
-=======
         if ($this->request->is('post')) {
             pr($this->request->data);
             if (isset($this->request->data['Fightermove']))
-            $this->Fighter->doMove(1, $this->request->data['Fightermove']['direction']);
+                $this->Fighter->doMove(1, $this->request->data['Fightermove']['direction']);
             if (isset($this->request->data['ChangeLevel']))
-            $this->Fighter->changeLevel(1, $this->request->data['ChangeLevel']['level']);
+                $this->Fighter->changeLevel(1, $this->request->data['ChangeLevel']['level']);
+            if (isset($this->request->data['Fighterattack']))
+                $this->Fighter->doAttack($this->request->data['Fighterattack']['id'], $this->request->data['Fighterattack']['id2'], $this->request->data['Fighterattack']['direction']);
+
         }
 
-        $this->set('raw',$this->Fighter->find('all'));
-
+        $this->set('raw', $this->Fighter->find('all'));
 
     }
-    
-     public function chooseAvatar()
+
+    public function chooseAvatar()
     {
         $fighterList = $this->Fighter->find('all', array('fields' => array('id', 'name')));
-        
-        $choicelist=array();
-        foreach( $fighterList as $key => $value)
-        {
-           $choicelist[$value['Fighter']['id']] = $value['Fighter']['name'];
+
+        $choicelist = array();
+        foreach ($fighterList as $key => $value) {
+            $choicelist[$value['Fighter']['id']] = $value['Fighter']['name'];
         }
         $this->set('fighterList', $choicelist);
         $this->render('/Arena/fighter_choice');
-        
-        if($this->request->is('post'))
-        {
-            $fighter_id = $this->request->data['Fighterchoice']['fighter_choice']; 
-            $this->set('id',$fighter_id);
-            
+
+        if ($this->request->is('post')) {
+            $fighter_id = $this->request->data['Fighterchoice']['fighter_choice'];
+            $this->set('id', $fighter_id);
+
             $this->render();
-            
-            if (is_uploaded_file($_FILES['avatar']['tmp_name']))
-            {
+
+            if (is_uploaded_file($_FILES['avatar']['tmp_name'])) {
                 $imageName = $_FILES['avatar']['name'];
-                $this->set('imageName',$imageName);
-                
-                
-                if(move_uploaded_file(
+                $this->set('imageName', $imageName);
+
+
+                if (move_uploaded_file(
                     $_FILES['avatar']['tmp_name'],
-                     WWW_ROOT .'img\\uploads\\avatar_'.$fighter_id.".jpg"
-                ))
-                {
-                    echo "Le transfert s'est bien deroule";       
-                }
-                else 
+                    WWW_ROOT . 'img\\uploads\\avatar_' . $fighter_id . ".jpg"
+                )
+                ) {
+                    echo "Le transfert s'est bien deroule";
+                } else
                     echo "erreur sur le transfert";
 
                 // store the filename in the array to be saved to the db
             }
-        }   
->>>>>>> e19297bb4339db851a7c0ad7e7cef6899fb35aaa
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public function createchar()
-    {
-        //création
-        if ($this->request->is('post'))
-        {
-        $data = array(
-            'Fighter' => array(
-                'name' => $this->request->data['Createchar']['create_name'],
-                'player_id'    => '545f827c-576c-4dc5-ab6d-27c33186dc3e',
-                'coordinate_x'=> $this->request->data['Createchar']['create_coordx'],
-                'coordinate_y'=> $this->request->data['Createchar']['create_coordy'],
-                'level'=> $this->request->data['Createchar']['create_level'],
-                'xp'=>$this->request->data['Createchar']['create_xp'],
-                'skill_sight'=>$this->request->data['Createchar']['create_skillsight'],
-                'skill_strength'=>$this->request->data['Createchar']['create_skillstrength'],
-                'skill_health'=> $this->request->data['Createchar']['create_skillhealth'],
-                'current_health'=>$this->request->data['Createchar']['create_current_health'] ) );
-
-        $this->Fighter->create();
-
-
-
-       if( $this->Fighter->save($data))
-            { return $this->redirect('sight');
-            }
-
-      //  $this->Fighter->creation($this->request->data['Createchar']['create_name'],$this->request->data['Createchar']['create_playerid'],$this->request->data['Createchar']['create_coordx'],$this->request->data['Createchar']['create_coordy'],$this->request->data['Createchar']['create_level'],$this->request->data['Createchar']['create_xp'],$this->request->data['Createchar']['create_skillsight'],$this->request->data['Createchar']['create_skillstrenght'],$this->request->data['Createchar']['create_skillhealth'],$this->request->data['Createchar']['create_current_health']);
-
-
-
-      // $this->Fighter->deletechar(); Test fonction delete, à refaire
-
-
         }
 
+    }
+
+    public function createchar()
+    {
+        //création
+        if ($this->request->is('post')) {
+            $data = array(
+                'Fighter' => array(
+                    'name' => $this->request->data['Createchar']['create_name'],
+                    'player_id' => '545f827c-576c-4dc5-ab6d-27c33186dc3e',
+                    'coordinate_x' => $this->request->data['Createchar']['create_coordx'],
+                    'coordinate_y' => $this->request->data['Createchar']['create_coordy'],
+                    'level' => $this->request->data['Createchar']['create_level'],
+                    'xp' => $this->request->data['Createchar']['create_xp'],
+                    'skill_sight' => $this->request->data['Createchar']['create_skillsight'],
+                    'skill_strength' => $this->request->data['Createchar']['create_skillstrength'],
+                    'skill_health' => $this->request->data['Createchar']['create_skillhealth'],
+                    'current_health' => $this->request->data['Createchar']['create_current_health']));
+
+            $this->Fighter->create();
 
 
-
-
+            if ($this->Fighter->save($data)) {
+                return $this->redirect('sight');
+            }
+            //  $this->Fighter->creation($this->request->data['Createchar']['create_name'],$this->request->data['Createchar']['create_playerid'],$this->request->data['Createchar']['create_coordx'],$this->request->data['Createchar']['create_coordy'],$this->request->data['Createchar']['create_level'],$this->request->data['Createchar']['create_xp'],$this->request->data['Createchar']['create_skillsight'],$this->request->data['Createchar']['create_skillstrenght'],$this->request->data['Createchar']['create_skillhealth'],$this->request->data['Createchar']['create_current_health']);
+            // $this->Fighter->deletechar(); Test fonction delete, à refaire
+        }
+    }
 }
-
-<<<<<<< HEAD
-
-
-
-
-
-}
-?>
-=======
-?>
->>>>>>> e19297bb4339db851a7c0ad7e7cef6899fb35aaa
--- Model/Fighter.php
+++ Model/Fighter.php
@@ -1,160 +1,4 @@
-<<<<<<< HEAD
-    <?php
-
-    App::uses('AppModel', 'Model');
-
-    class Fighter extends AppModel {
-
-        public $displayField = 'name';
-
-        public $belongsTo = array(
-
-            'Player' => array(
-
-                'className' => 'Player',
-
-                'foreignKey' => 'player_id',
-
-            ),
-
-        );
-
-        function doMove($fighterId, $direction) // ATTENTION UTILISABLE QUE SUR LE FIGHTER EN COURS DE JEU
-        {
-            // récupérer la position et fixer l'id de travail
-            $datas = $this->read(null, $fighterId);
-
-            // falre la modif
-            if ($direction == 'north')
-                $this->set('coordinate_x', $datas['Fighter']['coordinate_x'] + 1);
-            elseif ($direction == 'south')
-                $this->set('coordinate_x', $datas['Fighter']['coordinate_x'] - 1);
-            elseif ($direction == 'east')
-                $this->set('coordinate_y', $datas['Fighter']    ['coordinate_y'] + 1);
-            elseif ($direction == 'west')
-                $this->set('coordinate_y', $datas['Fighter']['coordinate_y'] - 1);
-            else
-                return false;
-
-            // sauver la modif
-            $this->save();
-            return true;
-        }
-
-/* // TEST FONCTION DELETE
-        public function deletechar(){
-
-echo "deletechar ici";
-            if( $this->Fighter->deleteAll($this->request->data($this->requet->data['Delete']['delete'])))
-            {
-                echo "succes";
-            }else
-            {
-                echo"fail";
-            }
-
-
-        }*/
-
-
-        function doAttack($id,$id2,$direction)
-        {
-           // On recupe l'id du méchant.
-           $datas= $this->read(null,$id);
-           $datas2=$this->read(null,$id2);
-
-
-            switch($direction){ // tention
-                case "east": {
-
-                                 echo $datas2['Fighter']['coordinate_x'];
-                                 echo $datas['Fighter']['coordinate_x']+1;
-
-                    if ($datas['Fighter']['coordinate_x']+1==$datas2['Fighter']['coordinate_x'])
-                 {
-                    $this->set('current_health', $datas['Fighter']['current_health']-1);
-                     echo "Succes";
-
-                 }else{
-                    echo "Raté";
-                }
-
-            }break;
-                case "west": {
-            if ($datas['Fighter']['coordinate_x']-1==$datas2['Fighter']['coordinate_x'])
-            {
-            $this->set('current_health', $datas['Fighter']['current_health']-1);
-                echo "Succes";
-
-
-            }else{
-                        echo "Raté";
-                    }
-              }
-                break;
-         //  $this->set('current_health', $datas['Fighter']['current_health']-1);
-                case "north" : {
-                    if ($datas['Fighter']['coordinate_y']+1==$datas2['Fighter']['coordinate_y'])
-                    {
-                        $this->set('current_health', $datas['Fighter']['current_health']-1);
-                        echo "Succes";
-
-
-                    }else{
-                        echo "Raté";
-                    }
-
-
-
-                }
-            break;
-
-                case "south" : {
-
-                    if ($datas['Fighter']['coordinate_y']-1==$datas2['Fighter']['coordinate_y'])
-                    {
-                        $this->set('current_health', $datas['Fighter']['current_health']-1);
-                        echo "Succes";
-
-
-                    }else{
-                        echo "Raté";
-                    }
-                }
-                    break;
-            }
-
-
-
-
-           $this->save();
-            return true;
-
-
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-=======
 <?php
-/**
- * Created by PhpStorm.
- * User: Robinson
- * Date: 14/11/14
- * Time: 14:20
- */
 
 App::uses('AppModel', 'Model');
 
@@ -164,50 +8,114 @@ class Fighter extends AppModel
     public $displayField = 'name';
 
     public $belongsTo = array(
-
         'Player' => array(
-
             'className' => 'Player',
-
             'foreignKey' => 'player_id',
-
         ),
-
     );
 
-    public function doMove($fighterId, $direction)
+    function doMove($fighterId, $direction) // ATTENTION UTILISABLE QUE SUR LE FIGHTER EN COURS DE JEU
     {
-        // Catch good object
+        // récupérer la position et fixer l'id de travail
         $datas = $this->read(null, $fighterId);
-        // Do modification on direction
+
+        // falre la modif
         if ($direction == 'north')
-            $this->set('coordinate_x', $datas['Fighter']['coordinate_x'] + 1);
-        elseif ($direction == 'south')
-            $this->set('coordinate_x', $datas['Fighter']['coordinate_x'] - 1);
-        elseif ($direction == 'east')
             $this->set('coordinate_y', $datas['Fighter']['coordinate_y'] + 1);
-        elseif ($direction == 'west')
+        elseif ($direction == 'south')
             $this->set('coordinate_y', $datas['Fighter']['coordinate_y'] - 1);
+        elseif ($direction == 'east')
+            $this->set('coordinate_x', $datas['Fighter']['coordinate_x'] + 1);
+        elseif ($direction == 'west')
+            $this->set('coordinate_x', $datas['Fighter']['coordinate_x'] - 1);
         else
             return false;
 
-        // Save modification
+        // sauver la modif
         $this->save();
         return true;
+    }
 
+    /* // TEST FONCTION DELETE
+            public function deletechar(){
+
+    echo "deletechar ici";
+                if( $this->Fighter->deleteAll($this->request->data($this->requet->data['Delete']['delete'])))
+                {
+                    echo "succes";
+                }else
+                {
+                    echo"fail";
+                }
+
+
+            }*/
+
+
+    function doAttack($id, $id2, $direction)
+    {
+        // On recupe l'id du méchant.
+        $datas = $this->read(null, $id);
+
+        $datas2 = $this->read(null, $id2);
+
+        switch ($direction) { // tention
+            case "east":
+            {
+                echo $datas2['Fighter']['coordinate_x'];
+                echo $datas['Fighter']['coordinate_x'] + 1;
+
+                if ($datas['Fighter']['coordinate_x'] + 1 == $datas2['Fighter']['coordinate_x'])
+                {
+                    $this->set('current_health', $datas2['Fighter']['current_health'] - 1);
+                    echo "Succes";
+                } else {
+                    echo "Raté";
+                }
+            }
+                break;
+            case "west":
+            {
+                if ($datas['Fighter']['coordinate_x'] - 1 == $datas2['Fighter']['coordinate_x']) {
+                    $this->set('current_health', $datas2['Fighter']['current_health'] - 1);
+                    echo "Succes";
+                } else {
+                    echo "Raté";
+                }
+            }
+                break;
+            //  $this->set('current_health', $datas['Fighter']['current_health']-1);
+            case "north" :
+            {
+                if ($datas['Fighter']['coordinate_y'] + 1 == $datas2['Fighter']['coordinate_y']) {
+                    $this->set('current_health', $datas2['Fighter']['current_health'] - 1);
+                    echo "Succes";
+                } else {
+                    echo "Raté";
+                }
+            }
+                break;
+
+            case "south" :
+            {
+                if ($datas['Fighter']['coordinate_y'] - 1 == $datas2['Fighter']['coordinate_y']) {
+                    $this->set('current_health', $datas2['Fighter']['current_health'] - 1);
+                    echo "Succes";
+                } else {
+                    echo "Raté";
+                }
+            }
+                break;
+        }
+        $this->save();
+        return true;
     }
 
     public function changeLevel($fighterId, $level)
     {
-
         $this->id = $fighterId;
         $this->saveField('level', $level);
 
         return true;
-
     }
-
 }
-
-        
->>>>>>> e19297bb4339db851a7c0ad7e7cef6899fb35aaa
