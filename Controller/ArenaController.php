de2ee909b8361e9a6bc60235b92c43e30d22ab3a
robinson Roy <robinson.roy@me.com>
2014-11-17 11:50:44 +0100

    delete branch

--- Controller/ArenaController.php
+++ Controller/ArenaController.php
@@ -44,7 +44,57 @@ class ArenaController extends AppController
 
     public function sight()
     {
-        //die('test');
+        if ($this->request->is('post')) {
+            pr($this->request->data);
+            if (isset($this->request->data['Fightermove']))
+            $this->Fighter->doMove(1, $this->request->data['Fightermove']['direction']);
+            if (isset($this->request->data['ChangeLevel']))
+            $this->Fighter->changeLevel(1, $this->request->data['ChangeLevel']['level']);
+        }
+
+        $this->set('raw',$this->Fighter->find('all'));
+
+
+    }
+    
+     public function chooseAvatar()
+    {
+        $fighterList = $this->Fighter->find('all', array('fields' => array('id', 'name')));
+        
+        $choicelist=array();
+        foreach( $fighterList as $key => $value)
+        {
+           $choicelist[$value['Fighter']['id']] = $value['Fighter']['name'];
+        }
+        $this->set('fighterList', $choicelist);
+        $this->render('/Arena/fighter_choice');
+        
+        if($this->request->is('post'))
+        {
+            $fighter_id = $this->request->data['Fighterchoice']['fighter_choice']; 
+            $this->set('id',$fighter_id);
+            
+            $this->render();
+            
+            if (is_uploaded_file($_FILES['avatar']['tmp_name']))
+            {
+                $imageName = $_FILES['avatar']['name'];
+                $this->set('imageName',$imageName);
+                
+                
+                if(move_uploaded_file(
+                    $_FILES['avatar']['tmp_name'],
+                     WWW_ROOT .'img\\uploads\\avatar_'.$fighter_id.".jpg"
+                ))
+                {
+                    echo "Le transfert s'est bien deroule";       
+                }
+                else 
+                    echo "erreur sur le transfert";
+
+                // store the filename in the array to be saved to the db
+            }
+        }   
     }
 }
 
