e19297bb4339db851a7c0ad7e7cef6899fb35aaa
Dex <a.sambres@gmail.com>
2014-11-17 11:47:56 +0100

    rajout chooseAvaar

--- Controller/ArenaController.php
+++ Controller/ArenaController.php
@@ -56,7 +56,46 @@ class ArenaController extends AppController
 
 
     }
+    
+     public function chooseAvatar()
+    {
+        $fighterList = $this->Fighter->find('all', array('fields' => array('id', 'name')));
+        
+        $choicelist=array();
+        foreach( $fighterList as $key => $value)
+        {
+           $choicelist[$value['Fighter']['id']] = $value['Fighter']['name'];
+        }
+        $this->set('fighterList', $choicelist);
+        $this->render('/Arena/fighter_choice');
+        
+        if($this->request->is('post'))
+        {
+            $fighter_id = $this->request->data['Fighterchoice']['fighter_choice']; 
+            $this->set('id',$fighter_id);
+            
+            $this->render();
+            
+            if (is_uploaded_file($_FILES['avatar']['tmp_name']))
+            {
+                $imageName = $_FILES['avatar']['name'];
+                $this->set('imageName',$imageName);
+                
+                
+                if(move_uploaded_file(
+                    $_FILES['avatar']['tmp_name'],
+                     WWW_ROOT .'img\\uploads\\avatar_'.$fighter_id.".jpg"
+                ))
+                {
+                    echo "Le transfert s'est bien deroule";       
+                }
+                else 
+                    echo "erreur sur le transfert";
 
+                // store the filename in the array to be saved to the db
+            }
+        }   
+    }
 }
 
 ?>
